0a272045d569000c7ac09dd3d1f0639e4baec818
Noelia Belen Lopez <lopeznoeliab@gmail.com>
2022-03-20 18:23:41 +0100

    Update env file

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,8 +6,6 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-    uint256[] gasPrice;
-
 
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
@@ -15,15 +13,11 @@ contract WavePortal {
 
     function wave() public {
         totalWaves += 1;
-        gasPrice.push(tx.gasprice);
         console.log("%s has waved!", msg.sender);
     }
 
     function getTotalWaves() public view returns (uint256) {
         console.log("We have %d total waves!", totalWaves);
-        for (uint i = 0; i < gasPrice.length; i++) {
-            console.log("%d", gasPrice[i]);
-        }
         return totalWaves;
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require('dotenv').config({path:__dirname+'/.env'})
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY]
+    },
+  },
 };
